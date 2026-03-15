92c346e4b6a00e6c18e8661c0ab491fce719bfe8
“thuta” <tripletthuta@gmail.com>
2026-03-15 14:51:53 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -342,3 +342,11 @@ print(chr(48))
 #문자에 대한 아스키코드 값 출력
 print(ord('a'))
 print(ord('0'))
+
+#개책 x가 가진 멤버 변수와 멤버 함수 보여주기
+print(dir([1, 2, 3]))   
+print(dir({'1':'a'}))
+print(dir(1))
+
+print(int('3'))
+print(str(3))
