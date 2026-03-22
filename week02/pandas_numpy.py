8fc359c2b569ceb46fb3ed2b6e0b0e52a9782fa2
“thuta” <tripletthuta@gmail.com>
2026-03-22 21:20:01 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -22,3 +22,5 @@ file_url='https://media.githubusercontent.com/media/musthave-ML10/data_source/ma
 sample_df = pd.read_csv(file_url, index_col=0)
 print(sample_df.head())
 print(sample_df['var_5'])
+
+#print(sample)
