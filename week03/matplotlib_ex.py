9c46c687285145ab59b044fb18eb90b1d0a96838
“thuta” <tripletthuta@gmail.com>
2026-03-29 14:09:24 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -175,4 +175,14 @@ plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
                  color= 'purple', alpha=0.9, label= 'Survived')
 
 
-plt.fill_between(survived_counts.index.astype(str), survived_counts[0], color='hotpink', alpha=0.6, label='Not Survived')
+plt.fill_between(survived_counts.index.astype(str), survived_counts[0], 
+                 color='hotpink', alpha=0.6, label='Not Survived')
+
+plt.title('Survival by Age Group on Titanic')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+plt.legend()
+plt.grid(True)
+plt
+
