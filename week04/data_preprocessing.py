18151a7d0eaff761c69bfae6ba9bb9b6c7c6be01
“thuta” <tripletthuta@gmail.com>
2026-04-05 14:05:53 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -73,3 +73,8 @@ housing_cat.head(8)
 from sklearn.preprocessing import OneHotEncoder
 
 ordinal_encoder =OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_trasform(housing_cat)
+
+housing_cat_encoded[:8]
+
+ordinal_encoder.categories_
