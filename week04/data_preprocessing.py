3ab13a22b1bc1a72809199712ac0bf1c0557f2df
“thuta” <tripletthuta@gmail.com>
2026-04-05 14:06:53 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -70,7 +70,7 @@ housing_labels = housing_labels.iloc[outlier_pred == 1]
 housing_cat = housing[["ocean_proximity"]]
 housing_cat.head(8)
 
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
 
 ordinal_encoder =OrdinalEncoder()
 housing_cat_encoded = ordinal_encoder.fit_trasform(housing_cat)
