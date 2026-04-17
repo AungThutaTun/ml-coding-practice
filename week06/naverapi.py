c9eb139dfadb662dd90cb0ab59b9bcbb5a97a84c
“thuta” <tripletthuta@gmail.com>
2026-04-17 22:58:25 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -73,11 +73,13 @@ def getNaverSearch(node, srcText, start, display) :
         description = post['description']
         org_link = post['originallink']
         link = post['link']
+        
         #%a:짧은 형식의 요일 이름 (에, 'Mon', 'Tue', 'Wed', ...)
         #%d:일 (예, 01,02,03,...)
         #%b:짧은 형식의 월 이름 (예, 'Jan', 'Feb', 'Mar', ...)
         pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
         pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
+        
         jsonResult.append({'cnt': cnt,
                            'title': title,
                            'description': description,
