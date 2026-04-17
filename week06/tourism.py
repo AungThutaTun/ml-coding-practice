acb43f67844019c9c5ed23c8cd952969b62c2d10
“thuta” <tripletthuta@gmail.com>
2026-04-17 23:52:09 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -59,4 +59,7 @@ def getTourismStatesService(nat_cd, ed_cd, nStartYear, nEndYear):
 
 def getTourismStatesItem(yyyymm, nat_cd, ed_cd):
     servide_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
-    parameters = ""
+    parameters = "?_type=json&servideKey=" + ServiceKey #인증키
+    parameters += "&YM=" + yyyymm 
+    parameters += "&NAT_CD=" + nat_cd
+    parameters += "&ED_CD=" + ed_cd
