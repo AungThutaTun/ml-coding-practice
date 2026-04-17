76fbb9cccb43763e3e070eedf8bac7768da7bb6c
“thuta” <tripletthuta@gmail.com>
2026-04-17 15:11:22 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -20,7 +20,7 @@ def main() :
     while ((jsonResponse != None) and (jsonResponse['display'] != 0) :
         for post in jsonResponse['items'] :
             cnt += 1
-            jsonResult.append(post)
+            jsonResult.append(post, jsonResult, cnt)   #[CODE 3]
         
         start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node, srcText, start, 100)  #[CODE 2]
