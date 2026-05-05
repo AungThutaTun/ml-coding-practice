d82aa83d8c4feaf65fb56834a1ac3fc34866d702
“thuta” <tripletthuta@gmail.com>
2026-05-06 00:08:34 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -129,4 +129,5 @@ netflix['listed_in'].str.split(', ', expand=True).stack()
 # 에시) 인ㄷㅅ스 1의 listed_in 열 값 : International TV Shows, TV Dramas, TV Mysteries
 genres = netflix['listed_in'].str.split(', ', expand=True).stack().value_counts()
 genres
+
 plt.figure(figsize=(12,6))
