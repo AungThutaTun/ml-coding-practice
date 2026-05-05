1f6d3b5a193f155724a6ef8a83d717f98c249828
“thuta” <tripletthuta@gmail.com>
2026-05-05 14:22:02 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -39,7 +39,7 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000],y[60000:]
 y_train_5 = (y_train == '5') #5 는 True고 , 다른 숫자는 모두 False
 y_test_5 = (y_test == '5')
 
-
+from sklearn
 
 
 
