cc4905c965da7fb9aa201fe3448bd115a51a3d49
“thuta” <tripletthuta@gmail.com>
2026-05-05 14:26:02 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -39,7 +39,16 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000],y[60000:]
 y_train_5 = (y_train == '5') #5 는 True고 , 다른 숫자는 모두 False
 y_test_5 = (y_test == '5')
 
-from sklearn
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state =42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predicct([some_digit])
+
+#성능 축장 - 교차 검증을 사용한 정확도 축정
+from sklearn.mode_selection import cross_val_score
+
 
 
 
