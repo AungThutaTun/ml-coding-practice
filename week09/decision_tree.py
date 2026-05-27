a6d5267a54f010854112a54a62cf79e72bdf46de
“thuta” <tripletthuta@gmail.com>
2026-05-27 23:29:07 +0900

    first commit

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -31,4 +31,13 @@ print(tree_clf.predict([[5,1.5]]))
 
 from sklearn.datasets import make_moons
 
-X_moons,
+X_moons, y_moons = make_moons(n_samples=150, noise=0.2, random_state=42)
+
+tree_clf1 = DecisionTreeClassifier(random_state=42)
+tree_clf2 = DecisionTreeClassifier(min_samples_leaf=5, random_state=42)
+tree_clf1.fit(X_moons, y_moons)
+tree_clf2.fit(X_moons, y_moons)
+
+X_moons_test, y_moons_test = make_moons(n_samples=1000 , noise=0.2, random_state=43)
+
+
